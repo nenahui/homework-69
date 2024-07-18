6d78b21a394f2cb0710583a6e8d6dbce901ae886
nenahui <kanatraccoon@gmail.com>
2024-07-18 23:58:56 +0600

    Final improvements to the application, namely added error handling, etc.

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,3 +1,9 @@
+import { Typography } from 'antd';
+
 export const Home = () => {
-  return <>Home Page</>;
+  return (
+    <Typography.Text type={'secondary'}>
+      Enter the show name in the input field and select the show
+    </Typography.Text>
+  );
 };
--- src/containers/Show/Show.tsx
+++ src/containers/Show/Show.tsx
@@ -3,10 +3,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { AppDispatch, RootState } from '../../app/store';
 import { useEffect } from 'react';
 import { fetchShow } from '../../store/homeThunks';
-import { Flex, Typography, Image, Empty } from 'antd';
+import { Flex, Typography, Image, Empty, Spin } from 'antd';
 
 export const Show = () => {
   const show = useSelector((state: RootState) => state.home.show);
+  const isLoading = useSelector((state: RootState) => state.home.isLoading);
   const dispatch: AppDispatch = useDispatch();
   const { showId } = useParams();
 
@@ -20,13 +21,29 @@ export const Show = () => {
     return <Empty description={'No data found for this show'} />;
   }
 
+  if (isLoading) {
+    return <Spin className={'spinner'} />;
+  }
+
   return (
     <Flex gap={'middle'} align={'start'}>
-      <div>
-        <Image src={show.image.original} alt={show.name + ' image'} />
-        <Typography.Text style={{ fontSize: 20 }}>{show.name}</Typography.Text>
+      <div style={!show.image ? { width: 700 } : { width: 'auto' }}>
+        {show.image ? (
+          <Image src={show.image.original} alt={show.name + ' image'} />
+        ) : (
+          <Empty description={`${show.name} image not found…`} />
+        )}
       </div>
-      <Typography.Paragraph>{show.summary}</Typography.Paragraph>
+      <div>
+        <Typography.Text style={{ fontSize: 20 }}>{show.name}</Typography.Text>
+        <Typography.Paragraph>
+          {show.summary ? (
+            <p dangerouslySetInnerHTML={{ __html: show.summary }} />
+          ) : (
+            'Unfortunately, our database does not contain a description of this show.…'
+          )}
+        </Typography.Paragraph>
+      </div>
     </Flex>
   );
 };
